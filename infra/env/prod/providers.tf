76df44019da58d7307f4e73eb8db8872f1dce3ae
veeresh-git <veeresh.alladi8@gmail.com>
2026-03-27 14:10:30 +0530

    adding outputs.tf

--- infra/env/prod/providers.tf
+++ infra/env/prod/providers.tf
@@ -11,16 +11,36 @@ provider "aws" {
 #  name = module.eks.cluster_name
 #}
 
-#provider "kubernetes" {
-#  host                   = data.aws_eks_cluster.this.endpoint
-#  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#  token                  = data.aws_eks_cluster_auth.this.token
-#}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-#provider "helm" {
-#  kubernetes {
-#    host                   = data.aws_eks_cluster.this.endpoint
-#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-#    token                  = data.aws_eks_cluster_auth.this.token
-#  }
-#}
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name
+      ]
+    }
+  }
+}
